42a8032a63d2f477af1a96353bf45bd182013d61
dicke <emmad47@uw.edu>
2019-11-18 13:46:09 -0800

    shiny outline

--- shiny/sleep_analysis/app.R
+++ shiny/sleep_analysis/app.R
@@ -9,26 +9,40 @@
 
 
 library(shiny)
+input_list <- c("career path", "education level", "health", "self-love")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Why Sleep when you have Netflix?"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    # tabs at the top
+    tabsetPanel(
+        type = "tabs", id = "nav_bar",
+        
+        # individual page
+        tabPanel(
+            "Introduction",
+            htmlOutput("intro"),
+            h2("Introduction"),
+            p("This is our project blah blah")
         ),
-
+        
+        tabPanel(
+            "University of Washington",
+            htmlOutput("UW")
+        ),
+        
+        tabPanel(
+            "About",
+            htmlOutput("about")
+        ),
+        
+        
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+         
         )
     )
 )
@@ -36,14 +50,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
