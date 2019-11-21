ba4eabcc0cf6dd517c8706254b1fc658b87b247c
joceboro-1838720 <joceboro@gmail.com>
2019-11-20 18:36:50 -0800

    add comments and organized code more

--- analysis.R
+++ analysis.R
@@ -43,7 +43,7 @@ graph_two_categoricals <- function(x, fill, x_level, fill_level,
       y = "Percent of People"
     ) +
     guides(fill = guide_legend(title = fill_label)) +
-    scale_fill_brewer(palette=color)
+    scale_fill_brewer(palette = color)
 }
 
 
@@ -51,6 +51,12 @@ graph_two_categoricals <- function(x, fill, x_level, fill_level,
 
 # Hours of sleep frequency Pie chart (performance)____________________
 
+# function that sums specified columns
+take_sum <- function(col) {
+  sum(hours_of_sleep_freq[col, "freq"])
+}
+
+# filter the data
 hours_of_sleep_freq <- performance %>%
   select(q7h) %>%
   filter(q7h <= 11) %>%
@@ -59,27 +65,31 @@ hours_of_sleep_freq <- performance %>%
     freq = length(q7h)
   )
 
-take_sum <- function(col) {
-  sum(hours_of_sleep_freq[col, "freq"])
-}
-
+# put it into intervals
 intervaled_freq <- c(
   take_sum(c(9, 10, 11)), take_sum(c(1, 2, 3)), take_sum(8),
   take_sum(7), take_sum(6), take_sum(5), take_sum(4)
 )
 
+# levels (text answers)
 ranges <- c(
   "more than 9", "less than 4", "8-9", "7-8", "6-7",
   "5-6", "4-5"
 )
+
+# array for specifying what index to start on in
+# for loop
 counts <- c(
   0, intervaled_freq[1], sum(intervaled_freq[1:2]),
   sum(intervaled_freq[1:3]), sum(intervaled_freq[1:4]),
   sum(intervaled_freq[1:5]), sum(intervaled_freq[1:6])
 )
 
+# array to fill
 sleep_ranges_initial <- c(1:sum(intervaled_freq))
 
+# translates the numbers that represent the multiple
+# choice answer into the text answer
 for (i in seq(1, length(intervaled_freq))) {
   for (j in seq(1, intervaled_freq[i])) {
     sleep_ranges_initial[j + counts[i]] <- ranges[i]
@@ -88,18 +98,21 @@ for (i in seq(1, length(intervaled_freq))) {
 
 sleep_ranges <- data.frame(ranges = sleep_ranges_initial)
 
+# add label and freq columns
 sleep_ranges <- sleep_ranges %>%
   group_by(ranges) %>%
   count() %>%
   ungroup() %>%
   mutate(per = `n` / sum(`n`)) %>%
-  arrange(desc(ranges))
+  arrange(desc(ranges)) %>%
+  mutate(
+    label = paste0(
+      ranges, " (",
+      scales::percent(per), ")"
+    )
+  )
 
-sleep_ranges$label <- paste0(
-  ranges, " (",
-  scales::percent(sleep_ranges$per), ")"
-)
-
+# create a pie chart
 ggplot(data = sleep_ranges) +
   geom_bar(aes(x = "", y = per, fill = ranges),
     stat = "identity", width = 1
@@ -117,23 +130,13 @@ ggplot(data = sleep_ranges) +
   ) +
   guides(fill = guide_legend(title = "Hrs of Sleep")) +
   labs(title = "Typical Hours of Sleep on a Workday") +
-  scale_fill_brewer(palette="Purples")
-
-
-# Percent of people that get less than 8 hrs of sleep (79%)
-
-percent_less_than_8 <- sum(
-  intervaled_freq[4:7],
-  intervaled_freq[2]
-) / sum(intervaled_freq)
-
-
+  scale_fill_brewer(palette = "Purples")
 
 
 
 # Hours of sleep correlated to income (performance data)___________
 
-
+# filter data
 hrs_income <- performance %>%
   filter(q7h <= 9, q7h >= 4, d8 <= 7)
 
@@ -152,17 +155,48 @@ income_level <- c(
   "$50,001 - $75,000", "$75,001 - $100,000",
   "More than $100,000"
 )
+
+# create a stacked bar chart
 graph_two_categoricals(
   hrs, income_range, hrs_level,
   income_level,
   "Typical Workday Hours of Sleep
                        Correlated to Income",
   "Hours of Sleep",
-  "Income Range", 
+  "Income Range",
   "Greens"
 )
 
-# Interesting stat, chance of making more than 75,000 on 5 and 8
+# hours of sleep correlated to stress levels (pain data) ________________
+
+# filter data
+hrs_stress <- pain %>%
+  filter(Q6_HoursB <= 11, Q6_HoursB >= 3, Q13_b <= 5, Q13_b >= 1)
+
+hrs2 <- sapply(hrs_stress$Q6_HoursB, toString)
+
+stress <- sapply(hrs_stress$Q13_b, toString)
+
+hrs2_level <- c(
+  "1", "2", "3", "4", "5", "6", "7",
+  "8", "9", "10", "11", "12"
+)
+
+stress_level <- c("None", "Mild", "Moderate", "Severe", "Very Severe")
+
+# create a stacked bar chart
+graph_two_categoricals(
+  hrs2, stress, hrs2_level, stress_level,
+  "Typical Workday Hours of Sleep
+                       Correlated to Stress",
+  "Hours of Sleep",
+  "Typical Stress Level",
+  "Reds"
+)
+
+# Interesting stats_______________________________________________________
+
+# chance of making more than 75,000 on 5 and 8
 # hours of sleep on workdays
 
 high_money_count <- hrs_income %>%
@@ -186,76 +220,9 @@ chance_5 <- hrs_income %>%
 
 diff_5_to_7 <- chance_7 - chance_5
 
-# hours of sleep correlated to stress levels (pain) ________________
+# Percent of people that get less than 8 hrs of sleep (79%)
 
-# Q6_HoursB
-# Q13_b
-
-hrs_stress <- pain %>%
-  filter(Q6_HoursB <= 11, Q6_HoursB >= 3, Q13_b <= 5, Q13_b >= 1)
-
-hrs2 <- sapply(hrs_stress$Q6_HoursB, toString)
-
-stress <- sapply(hrs_stress$Q13_b, toString)
-
-hrs2_level <- c(
-  "1", "2", "3", "4", "5", "6", "7",
-  "8", "9", "10", "11", "12"
-)
-
-stress_level <- c("None", "Mild", "Moderate", "Severe", "Very Severe")
-
-graph_two_categoricals(
-  hrs2, stress, hrs2_level, stress_level,
-  "Typical Workday Hours of Sleep
-                       Correlated to Stress",
-  "Hours of Sleep",
-  "Typical Stress Level",
-  "Reds"
-)
-
-# #Symptoms from lack of sleep in the workplace bar chart
-# (performance)___________________
-
-# take_4_5 <- function(dat, col) {
-#   filtered <- dat %>%
-#     select(col) %>%
-#     filter(dat[,col] == 4 | dat[,col] == 5)
-# }
-#
-# effectsA <- nrow(take_4_5(performance, "q44a"))
-# effectsB <- nrow(take_4_5(performance, "q44b"))
-# effectsC <- nrow(take_4_5(performance, "q44c"))
-# effectsD <- nrow(take_4_5(performance, "q44d"))
-# effectsE <- nrow(take_4_5(performance, "q44e"))
-# effectsF <- nrow(take_4_5(performance, "q44f"))
-# effectsG <- nrow(take_4_5(performance, "q44g"))
-# effectsH <- nrow(take_4_5(performance, "q44h"))
-#
-# freq_sleep_effects <- c(effectsA, effectsB, effectsC, effectsD,
-#                         effectsE, effectsF, effectsG, effectsH)
-#
-# names <- c("Find it difficult to concentrate",
-# "Have trouble organizing work",
-# "Become impatient with others",
-# "Avoid interactions with others",
-# "Become bored at work",
-# "Have to do a job over due to mistakes",
-# "Notice your productivity was lower than expected",
-# "Fail to finish assigned tasks")
-#
-# sleep_effects <- data.frame(freq_sleep_effects, names)
-#
-# ggplot(sleep_effects,
-#   mapping = aes(
-#     x = factor(names, level = names),
-#     y = freq_sleep_effects
-#   )
-# ) +
-#   geom_col() +
-#   coord_flip() +
-#   labs(
-#     title = "The Effects of Sleep on Work Life",
-#     y = "Number of People",
-#     x = "Consequences from Lack of Sleep"
-#   )
+percent_less_than_8 <- sum(
+  intervaled_freq[4:7],
+  intervaled_freq[2]
+) / sum(intervaled_freq)
