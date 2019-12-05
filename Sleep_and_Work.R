0a6563083b55b875c2c334c726e6dd04e0605aba
joceboro-1838720 <46583375+joceboro-1838720@users.noreply.github.com>
2019-12-04 23:32:01 -0800

    copy

--- Sleep_and_Work.R
+++ Sleep_and_Work.R
@@ -1,7 +1,8 @@
-sleep_and_workplace_page <-  tabPanel(
+# Sleep and Work page containing visual graphs and captions
+sleep_and_workplace_page <- tabPanel(
   "Sleep and Work",
   htmlOutput("sleep_workplace"),
-  
+
   sidebarLayout(
     sidebarPanel(
       h3("Sleep and Work"),
@@ -10,14 +11,16 @@ sleep_and_workplace_page <-  tabPanel(
         inputId = "workplace",
         label = "Typical Hours of Sleep vs...",
         selected = "Highest Education",
-        choices = list("Highest Education" = "education",
-                       "Income" = "income",
-                       "Sleeping and Driving" = "driving",
-                       "Caffine" = "caffine",
-                       "Quality of Life" = "quality")
+        choices = list(
+          "Highest Education" = "education",
+          "Income" = "income",
+          "Sleeping and Driving" = "driving",
+          "Caffine" = "caffeine",
+          "Quality of Life" = "quality"
+        )
       )
     ),
-    
+
     mainPanel(
       plotOutput(outputId = "workplace_graph"),
       h3(),
