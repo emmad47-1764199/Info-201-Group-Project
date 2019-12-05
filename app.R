43c1937777862ce475c7e7bba8fef8aa60993555
joceboro-1838720 <46583375+joceboro-1838720@users.noreply.github.com>
2019-12-04 23:34:07 -0800

    co;py

--- app.R
+++ app.R
@@ -10,10 +10,10 @@ source("about_us.R")
 
 
 ui <- fluidPage(
-  
+
   # Theme
   theme = shinytheme("superhero"),
-  
+
   # Application title
   titlePanel("The Importance of Sleep"),
 
@@ -34,62 +34,77 @@ ui <- fluidPage(
   )
 )
 
-
+# server
 server <- function(input, output) {
-  
   output$sleep_pie <- renderPlot({
     return(sleep_freq_pie)
   })
+  # renders graphs based on input for work page
   output$workplace_graph <- renderPlot({
     return(get_graph(input$workplace))
   })
+  # renders captions based on input for work page
   output$workplace_text <- renderText({
     return(get_text(input$workplace))
   })
+  # renders graphs based on input for health page
   output$health_graph <- renderPlot({
     return(get_graph(input$health))
   })
+  # renders captions based on input for health page
   output$health_text <- renderText({
     return(get_text(input$health))
   })
-  
-  output$Emma_photo <- renderImage({
-    list(src = "photos_of_us/Emma.jpg",
-         contentType = "image/jpg",
-         width = 400,
-         height = 400,
-         alt = "This is Emma's photo")
+
+  output$emma_photo <- renderImage({
+    list(
+      src = "photos_of_us/Emma.jpg",
+      contentType = "image/jpg",
+      width = 400,
+      height = 400,
+      alt = "This is Emma's photo"
+    )
   },
-  deleteFile = FALSE)
-  
-  output$Balqisa_photo <- renderImage({
-    list(src = "photos_of_us/Balqisa.JPG",
-         contentType = "image/jpg",
-         width = 300,
-         height = 400,
-         alt = "This is Balqisa's photo")
+  deleteFile = FALSE
+  )
+
+  output$balqisa_photo <- renderImage({
+    list(
+      src = "photos_of_us/Balqisa.JPG",
+      contentType = "image/jpg",
+      width = 300,
+      height = 400,
+      alt = "This is Balqisa's photo"
+    )
   },
-  deleteFile = FALSE)
-  
-  output$Jocelyn_photo <- renderImage({
-    list(src = "photos_of_us/Jocelyn.jpg",
-         contentType = "image/jpg",
-         width = 800,
-         height = 400,
-         alt = "This is Jocelyn's photo")
+  deleteFile = FALSE
+  )
+
+  output$jocelyn_photo <- renderImage({
+    list(
+      src = "photos_of_us/Jocelyn.jpg",
+      contentType = "image/jpg",
+      width = 800,
+      height = 400,
+      alt = "This is Jocelyn's photo"
+    )
   },
-  deleteFile = FALSE)
-  
-  output$Rae_photo <- renderImage({
-    list(src = "photos_of_us/Rae.JPG",
-         contentType = "image/jpg",
-         width = 550,
-         height = 400,
-         alt = "This is Rae's photo")
+  deleteFile = FALSE
+  )
+
+  output$rae_photo <- renderImage({
+    list(
+      src = "photos_of_us/Rae.JPG",
+      contentType = "image/jpg",
+      width = 550,
+      height = 400,
+      alt = "This is Rae's photo"
+    )
   },
-  deleteFile = FALSE)
+  deleteFile = FALSE
+  )
 }
 
 
-# Run the application
+# Runs the application
 shinyApp(ui = ui, server = server)
