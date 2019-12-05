042b31d2b575c1e760f597e136268828e889db34
joceboro-1838720 <46583375+joceboro-1838720@users.noreply.github.com>
2019-12-04 23:35:34 -0800

    copy

--- introduction.R
+++ introduction.R
@@ -1,34 +1,39 @@
+# introduction page containing a visual and overview
 intro_page <- tabPanel(
   "Introduction",
   htmlOutput("intro"),
-  
+
   sidebarLayout(
     sidebarPanel(
       h3("Overview"),
       p("Over three-fourths of the American population gets less than
-         8 hours of sleep on workdays, yet a lack of sleep can have 
+         8 hours of sleep on workdays, yet a lack of sleep can have
          serious effects such as poor health and a tougher lifestyle.
-         Oftentimes, people prioritize other responsibilities, such as
+         Oftentimes, people prioritize other responsibilities such as
          work, school, exercise and socialization over sleep, but at a
          certain level of lack of sleep, performance in these tasks are
-         diminished. This study will assess the tradeoff between getting
-        enough sleep and accomplishing these other activities."),
-      p("Use this application to learn about the importance of sleep on 
-         health, work performance, and happiness."),
-      p("This application uses the following two datasets: ",
+         diminished. This study will assess the benifits and consequences
+         to lifestyle, health, and happiness at different levels of sleep."),
+      p("Use this application to learn about the importance of sleep on
+         health, lifestyle, and happiness."),
+      p(
+        "This report uses the following two datasets: ",
         a("2015 Sleep and Pain",
-          href =
-            "https://www.sleepfoundation.org/professionals/sleep-america-polls/2015-sleep-and-pain"
+          href = paste0(
+            "https://www.sleepfoundation.org/professionals/sleep-",
+            "america-polls/2015-sleep-and-pain"
+          )
         ),
         " and ",
         a("2008 Sleep Performance and the Workplace",
-          href =
-            "https://www.sleepfoundation.org/professionals/sleep-america-polls/2008-sleep-performance-and-workplace"
+          href = paste0(
+            "https://www.sleepfoundation.org/professionals/sleep-",
+            "america-polls/2008-sleep-performance-and-workplace"
+          )
         ),
-        "."
       )
     ),
-    
+
     mainPanel(
       plotOutput(outputId = "sleep_pie"),
       h6("This graphic displays a pie chart of the proportion of people
